6a1aed8b34cc5405bd049c7bfc6d8528957f5e75
Ashique imran <imranashik50@gmail.com>
2017-12-10 21:47:26 +0600

    search.php changed for security

--- search.php
+++ search.php
@@ -1,2 +1,28 @@
-	  $input= mysqli_real_escape_string( $connect,$_POST["query"]);
-    $query = "SELECT * FROM placetable WHERE plcaeName LIKE '".$input."%'";
+ <?php
+ session_start();
+ $connect = mysqli_connect("localhost", "root", "", "travelguide");  
+ if(isset($_POST["query"]))  
+ {  
+      $output = '';  
+      $input= mysqli_real_escape_string( $connect,$_POST["query"]);
+      $query = "SELECT * FROM placetable WHERE plcaeName LIKE '".$input."%'";      $result = mysqli_query($connect, $query);  
+      $output = '<ul class="list-unstyled">';  
+      if(mysqli_num_rows($result) > 0)  
+      {  
+           while($row = mysqli_fetch_array($result))  
+           {  
+                $output .= '<li>'.$row["plcaeName"].'</li>';  
+           }  
+      }  
+      else  
+      {  
+           $output .= '<li>Not Found</li>';  
+      }  
+      $output .= '</ul>'; 
+
+      if(isset($_SESSION['user']) || isset($_SESSION['mail'])||isset($_POST["place"]))
+       {
+        echo $output;
+       }  
+ }  
+ ?> 
